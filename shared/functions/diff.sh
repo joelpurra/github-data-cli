00212f5e32e741abc1661c4e41a657e1778f09cc
Joel Purra <code+github@joelpurra.com>
2017-01-20 13:51:43 +0100

    Fix diffing of new or removed entries

--- shared/functions/diff.sh
+++ shared/functions/diff.sh
@@ -1,16 +1,23 @@
 function getJsonForDiffing() {
-	local filename="$1"
+	local -r filename="$1"
 	shift
-	local from="$1"
+	local -r from="$1"
 	shift
-	local to="$1"
+	local -r to="$1"
 	shift
 
-	cat "${configOutdir}/${from}/${filename}" "${configOutdir}/${to}/${filename}"
+	local -r fromPath="${configOutdir}/${from}/${filename}"
+	local -r toPath="${configOutdir}/${to}/${filename}"
+
+	[[ ! -f "$fromPath" ]] && die "Could not find diff 'from' path:" "$fromPath"
+
+	[[ ! -f "$toPath" ]] && die "Could not find diff 'to' path:" "$toPath"
+
+	cat "$fromPath" "$toPath"
 }
 
 function diffJsonTwoArraysOfValuePairObjectsArrays {
-	jq --slurp 'def objectValuePairToKeyValue: . | to_entries | [ { key: .[0].value, value: .[1].value } ] | from_entries; def objectValuePairToKeyValueArrayToObject: reduce .[] as $entry ({}; . + ($entry | objectValuePairToKeyValue)); (.[0] | objectValuePairToKeyValueArrayToObject) as $left | (.[1] | objectValuePairToKeyValueArrayToObject) as $right | $left + $right | keys | reduce .[] as $key ({}; . + { ($key): ($right[$key] - $left[$key]) }) | with_entries(select(.value != 0))'
+	jq --slurp 'def objectValuePairToKeyValue: . | to_entries | [ { key: .[0].value, value: .[1].value } ] | from_entries; def objectValuePairToKeyValueArrayToObject: reduce .[] as $entry ({}; . + ($entry | objectValuePairToKeyValue)); (.[0] | objectValuePairToKeyValueArrayToObject) as $left | (.[1] | objectValuePairToKeyValueArrayToObject) as $right | $left + $right | keys | reduce .[] as $key ({}; . + { ($key): (($right[$key] // 0) - ($left[$key] // 0)) }) | with_entries(select(.value != 0))'
 }
 
 function diffDateJson {
